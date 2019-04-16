f65e010c91e6c6df760b1e5f40f8090fbc503697
Tobias Gießler <15172111+tobiasgiessler@users.noreply.github.com>
2019-04-16 20:30:01 +0200

    First successful test.

--- src/main/java/com/huidi/groups/controller/RestController.java
+++ src/main/java/com/huidi/groups/controller/RestController.java
@@ -4,35 +4,45 @@ import com.huidi.groups.model.Group;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
-    @RequestMapping(value = "/groups", method = RequestMethod.GET, produces = "application/json")
-    public List<Group> firstPage() {
-        final Group group1 = new Group();
-        group1.setId("01");
-        group1.setName("Schachgruppe");
-        group1.setDescription("Eine Gruppe zum Schach spielen.");
-        final Group group2 = new Group();
-        group2.setId("02");
-        group2.setName("Sportgruppe");
-        group2.setDescription("Für alle die gerne fit sind.");
-        final Group group3 = new Group();
-        group3.setId("03");
-        group3.setName("Häkelgruppe");
-        group3.setDescription("Kinderkleidung selber machen.");
+    private final List<Group> groups;
+    private int lastId;
 
-        final List<Group> allGroups = new ArrayList<>();
-        allGroups.add(group1);
-        allGroups.add(group2);
-        allGroups.add(group3);
-
-        return allGroups;
+    public RestController() {
+        lastId = 0;
+        groups = new LinkedList<>();
     }
 
+    @RequestMapping(value = "/groups", method = RequestMethod.GET, produces = "application/json")
+    public List<Group> firstPage() {
+        return getGroups();
+    }
+
+    @RequestMapping(value = "/groups", method = RequestMethod.POST, produces = "application/json", consumes = "application/x-www-form-urlencoded")
+    public Group createGroup(@RequestParam(value = "name") String name, @RequestParam(value = "description") String description) {
+        final Group newGroup = new Group();
+        newGroup.setId(String.valueOf(getNextId()));
+        newGroup.setName(name);
+        newGroup.setDescription(description);
+
+        groups.add(newGroup);
+
+        return newGroup;
+    }
+
+    private List<Group> getGroups() {
+        return groups;
+    }
+
+    private int getNextId() {
+        return lastId++;
+    }
 }
--- src/test/java/com/huidi/groups/GroupsApplicationTests.java
+++ src/test/java/com/huidi/groups/GroupsApplicationTests.java
@@ -1,16 +1,42 @@
 package com.huidi.groups;
 
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GroupsApplicationTests {
 
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testGetGroups_noExistingGroups() throws Exception {
+		mockMvc.perform(get("/groups")).andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+		.andExpect(jsonPath("$").value(Matchers.empty()));
 	}
 
 }
