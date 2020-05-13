fcbed0f39ae9b7b0ad2640bb5ff59925dbfa2b28
Tobias Gießler <15172111+tobiasgiessler@users.noreply.github.com>
2020-05-13 19:28:35 +0200

    Add skeleton for test

---
+++ group-service/src/test/java/com/huidi/group/service/GroupServiceApplicationTest.java
@@ -0,0 +1,16 @@
+package com.huidi.group.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class GroupServiceApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
